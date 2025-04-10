d2ee8266e256010d542eb41b300e52186031b682
kimdaesun <daesun07250725@gmail.com>
2025-04-10 10:32:43 +0900

    v0.3 kiosk

--- week06_kiosk.py
+++ week06_kiosk.py
@@ -1,9 +1,10 @@
+prices = [1500, 2500]
 while True:
-    menu = input("1) 아이스 아메리카노 2) 카페 라떼 3) 주문종료 : ")
+    menu = input(f"1) 아이스 아메리카노 {prices[0]}원 2) 카페 라떼{prices[1]}원 3) 주문종료 : ")
     if menu == "1" :
-        print("아이스 아메리카노를 주문하셨습니다 가격은 2000원입니다")
+        print(f"아이스 아메리카노를 주문하셨습니다 가격은 {prices[0]}원입니다")
     elif menu == "2":
-        print("카페 라떼를 주문하셨습니다 가격은 2500원입니다")
+        print(f"카페 라떼를 주문하셨습니다 가격은 {prices[1]}원입니다")
     elif menu == "3":
         print("주문을 종료합니다")
         break
