998201f6d45b987d24462054b07a1febdc72dcd0
kimdaesun <daesun07250725@gmail.com>
2025-04-17 11:25:02 +0900

    v1.8kiosk.py

---
+++ temp.py
@@ -0,0 +1,9 @@
+# split
+texts = "우리는 국가와 국민에 충성을 다하는 대한민국 육군이다"
+texts_list = texts.split()
+print(texts_list)
+
+#join
+subjects = ['파이썬', '자바', 'C언어']
+subjects_string = ' / '.join(subjects)
+print(subjects_string)
